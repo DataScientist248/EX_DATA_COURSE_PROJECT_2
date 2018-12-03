281c1a7ab9e32955478bce2789fc032c8e678d3e
Chirag Subramanian <chirag1810@gmail.com>
2018-12-02 19:32:16 -0500

    plot4.R

---
+++ plot4.R
@@ -0,0 +1,17 @@
+library("ggplot2")
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+NEISCC <- merge(NEI, SCC, by="SCC")
+
+COAL  <- grepl("coal", NEISCC$Short.Name, ignore.case=TRUE)
+NEISCC_SUBSET <- NEISCC[COAL, ]
+YEARLY_TOTAL <- aggregate(Emissions ~ year, NEISCC_SUBSET, sum)
+png("plot4.png", width=640, height=480)
+g <- ggplot(YEARLY_TOTAL, aes(factor(year), Emissions))
+g <- g + geom_bar(stat="identity") +
+  xlab("year") +
+  ylab(expression('Total PM'[2.5]*" Emissions")) +
+  ggtitle('Total Emissions from coal sources from 1999 to 2008')
+print(g)
+dev.off()
